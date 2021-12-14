6ae2b7fe8bd9f75643ac3728a99fe816ff52cb14
cmj <chmin82@gmail.com>
2021-12-15 00:29:27 +0900

    1.4 volumeCreditsFor 함수 생성

--- statement.js
+++ statement.js
@@ -9,11 +9,7 @@ function statement(invoices, plays) {
   const format = new Intl.NumberFormat("en-US", {style: "currency", currency: "USD", minimumFractionDigits: 2}).format;
 
   for (let perf of invoices.performances) {
-
-    // 포인트를 적립한다
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    // 희극 관객 5명마다 추가 포인트를 제공한다.
-    if("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
+    volumeCredits += volumeCreditsFor(perf)
 
     // 청구 내역을 출력한다.
     result += `${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience}석)\n`;
@@ -48,6 +44,13 @@ function amountFor(aPerformance) {
 
 function playFor(aPerformance) {
   return plays[aPerformance.playID];
+}
+
+function volumeCreditsFor(perf) {
+  let volumeCredits = 0;
+  volumeCredits += Math.max(perf.audience - 30, 0);
+  if("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
+  return volumeCredits;
 }
 
 console.log(statement(invoices, plays));
