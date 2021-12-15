44f102639ce2fafada5e5ac303f5232833ba8639
cmj <chmin82@gmail.com>
2021-12-15 22:15:02 +0900

    1.4 volumeCredits 변수 제거하기

--- statement.js
+++ statement.js
@@ -4,17 +4,16 @@ const invoices = require('./invoices.json');
 
 function statement(invoices, plays) {
   let totalAmount = 0;
-  let volumeCredits = 0;
   let result = `청구 내역 (고객명: ${invoices.customer})\n`;
-  for (let perf of invoices.performances) {
-    volumeCredits += volumeCreditsFor(perf)
 
+  for (let perf of invoices.performances) {
     // 청구 내역을 출력한다.
     result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
     totalAmount += amountFor(perf);
   }
+
   result += `총액: ${usd(totalAmount)}\n`;
-  result += `적립 포인트: ${volumeCredits}점\n`;
+  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
   return result;
 }
 
@@ -57,6 +56,14 @@ function usd(aNumber) {
     currency: "USD",
     minimumFractionDigits: 2
   }).format(aNumber/100);
+}
+
+function totalVolumeCredits() {
+  let volumeCredits = 0;
+  for (let perf of invoice.performances) {
+    volumeCredits += volumeCreditsFor(perf);
+  }
+  return volumeCredits;
 }
 
 console.log(statement(invoices, plays));
