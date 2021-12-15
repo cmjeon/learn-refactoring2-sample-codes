229bf432bda3577737b63747dc4e002a87bf4724
cmj <chmin82@gmail.com>
2021-12-15 22:37:52 +0900

    1.4 totalAmount 변수 제거

--- statement.js
+++ statement.js
@@ -3,16 +3,14 @@ const plays = require('./plays.json');
 const invoices = require('./invoices.json');
 
 function statement(invoices, plays) {
-  let totalAmount = 0;
   let result = `청구 내역 (고객명: ${invoices.customer})\n`;
 
   for (let perf of invoices.performances) {
     // 청구 내역을 출력한다.
     result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
-    totalAmount += amountFor(perf);
   }
 
-  result += `총액: ${usd(totalAmount)}\n`;
+  result += `총액: ${usd(totalAmount())}\n`;
   result += `적립 포인트: ${totalVolumeCredits()}점\n`;
   return result;
 }
@@ -59,11 +57,19 @@ function usd(aNumber) {
 }
 
 function totalVolumeCredits() {
-  let volumeCredits = 0;
-  for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf);
+  let result = 0;
+  for (let perf of invoices.performances) {
+    result += volumeCreditsFor(perf);
   }
-  return volumeCredits;
+  return result;
+}
+
+function totalAmount() {
+  let result = 0;
+  for (let perf of invoices.performances) {
+    result += amountFor(perf);
+  }
+  return result;
 }
 
 console.log(statement(invoices, plays));
