d5411db6b46d9920c5a37e46960d7cc83875c428
cmj <chmin82@gmail.com>
2021-12-18 01:03:07 +0900

    1.8  createPerformanceCalculator 분기, TragedyCalculator, ComedyCalculator 생성

--- createStatementData.mjs
+++ createStatementData.mjs
@@ -31,7 +31,7 @@ function createStatementData(invoices, plays) {
   }
 
   function totalAmount(data) {
-    return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
+    return data.performances.reduce((total, p) => total + p.amount, 0);
   }
 
   function totalVolumeCredits(data) {
@@ -40,7 +40,14 @@ function createStatementData(invoices, plays) {
 }
 
 function createPerformanceCalculator(aPerformance, aPlay) {
-  return new PerformanceCalculator(aPerformance, aPlay);
+  switch(aPlay.type) {
+    case "tragedy":
+      return new TragedyCalculator(aPerformance, aPlay);
+    case "comedy":
+      return new ComedyCalculator(aPerformance, aPlay);
+    default:
+      throw new Error(`알 수 없는 장르: ${aPlay.type}`);
+  }
 }
 
 class PerformanceCalculator {
@@ -73,6 +80,27 @@ class PerformanceCalculator {
     let result = 0;
     result += Math.max(this.performance.audience - 30, 0);
     if ("comedy" === this.play.type) result += Math.floor(this.performance.audience / 5);
+    return result;
+  }
+}
+
+class TragedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 40000;
+    if (this.performance.audience > 30) {
+      result += 1000 * (this.performance.audience - 30);
+    }
+    return result;
+  }
+}
+
+class ComedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 30000;
+    if (this.performance.audience > 20) {
+      result += 10000 + 500 * (this.performance.audience - 20);
+    }
+    result += 300 * this.performance.audience;
     return result;
   }
 }
