2ec8c8393c13599d2eb9df414b423eef9f45706d
cmj <chmin82@gmail.com>
2021-12-17 20:26:11 +0900

    1.6 표현할 volumeCredits 옮기기

--- statement.js
+++ statement.js
@@ -12,6 +12,7 @@ function statement(invoices, plays) {
     const result = Object.assign({}, aPerformance); // 얕은 복사 수행
     result.play = playFor(result);
     result.amount = amountFor(result);
+    result.volumeCredits = volumeCreditsFor(result);
     return result;
   }
 
@@ -40,6 +41,13 @@ function statement(invoices, plays) {
     }
     return result;
   }
+
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0);
+    if("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
+    return result;
+  }
 }
 
 function renderPlainText(data, plays) {
@@ -63,7 +71,7 @@ function renderPlainText(data, plays) {
   function totalVolumeCredits() {
     let result = 0;
     for (let perf of data.performances) {
-      result += volumeCreditsFor(perf);
+      result += perf.volumeCredits;
     }
     return result;
   }
@@ -74,13 +82,6 @@ function renderPlainText(data, plays) {
       currency: "USD",
       minimumFractionDigits: 2
     }).format(aNumber/100);
-  }
-
-  function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-    if("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
-    return result;
   }
 
 }
