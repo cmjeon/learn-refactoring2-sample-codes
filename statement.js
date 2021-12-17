bddcd2a7098edc849b4f6afc8db6dbe299487f02
cmj <chmin82@gmail.com>
2021-12-17 20:16:54 +0900

    1.6 표현할 plays 정보를 statementData 로 옮기기

--- statement.js
+++ statement.js
@@ -5,8 +5,18 @@ const invoices = require('./invoices.json');
 function statement(invoices, plays) {
   const statementData = {};
   statementData.customer = invoices.customer;
-  statementData.performances = invoices.performances;
+  statementData.performances = invoices.performances.map(enrichPerformance);
   return renderPlainText(statementData, plays);
+
+  function enrichPerformance(aPerformance) {
+    const result = Object.assign({}, aPerformance); // 얕은 복사 수행
+    result.play = playFor(result);
+    return result;
+  }
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+  }
 }
 
 function renderPlainText(data, plays) {
@@ -16,6 +26,7 @@ function renderPlainText(data, plays) {
   }
   result += `총액: ${usd(totalAmount())}\n`;
   result += `적립 포인트: ${totalVolumeCredits()}점\n`;
+
   return result;
 
   function totalAmount() {
@@ -45,17 +56,13 @@ function renderPlainText(data, plays) {
   function volumeCreditsFor(aPerformance) {
     let result = 0;
     result += Math.max(aPerformance.audience - 30, 0);
-    if("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
+    if("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
     return result;
-  }
-
-  function playFor(aPerformance) {
-    return plays[aPerformance.playID];
   }
 
   function amountFor(aPerformance) {
     let result = 0;
-    switch(playFor(aPerformance).type) {
+    switch(aPerformance.play.type) {
       case "tragedy": // 비극
         result = 40000;
         if (aPerformance.audience > 30) {
@@ -70,7 +77,7 @@ function renderPlainText(data, plays) {
         result += 300 * aPerformance.audience;
         break;
       default:
-        throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
+        throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
     }
     return result;
   }
